63b66013c222971450709bf87b243a9e27657068
Evan Reeves <evanreeves22@gmail.com>
2018-06-22 03:29:42 +1000

    label and lazy culling

---
+++ MGUI/Controls/Label.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MGUI.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MGUI.Controls
+{
+    public class Label : Control
+    {
+        public string Text { get; set; } = string.Empty;
+        
+        public Label(Canvas canvas) : base(canvas)
+        {
+        }
+
+        public override void Draw(SpriteBatch batcher)
+        {
+            batcher.DrawString(Canvas.DefaultFont, Text, CanvasBounds.Location.ToVector2(), Color);
+        }
+    }
+}
---
+++ MGUI/Core/RenderTools.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MGUI.Core
+{
+    public class RenderTools
+    {
+        private readonly GraphicsDevice graphics;
+        private readonly Rectangle canvasBounds;
+        private RasterizerState rasterizerState;
+        private RasterizerState rasterizerStateNoScissor;
+        
+        //So drawing can be customized.
+
+        public SpriteSortMode SpriteSortMode { get; set; } = SpriteSortMode.Deferred;
+        public BlendState BlendState { get; set; } = null;
+        public SamplerState SamplerState { get; set; } = null;
+        public DepthStencilState DepthStencilState { get; set; } = null;
+        public Effect Effect { get; set; } = null;
+        
+        public RenderTools(GraphicsDevice graphics, Rectangle canvasBounds)
+        {
+            this.graphics = graphics;
+            this.canvasBounds = canvasBounds;
+            rasterizerState = new RasterizerState { ScissorTestEnable = true };
+
+            graphics.ScissorRectangle = canvasBounds;
+        }
+
+        public void Start(SpriteBatch batcher)
+        {
+            batcher.GraphicsDevice.ScissorRectangle = canvasBounds;
+            batcher.Begin(SpriteSortMode, BlendState, SamplerState, DepthStencilState, rasterizerState);
+        }
+
+        public void StartCull(SpriteBatch batcher, Rectangle bounds)
+        {
+            batcher.End();
+            batcher.GraphicsDevice.ScissorRectangle = bounds;
+            batcher.Begin(SpriteSortMode, BlendState, SamplerState, DepthStencilState, rasterizerState);
+        }
+
+        public void EndCull(SpriteBatch batcher)
+        {
+            batcher.End();
+            Start(batcher);
+        }
+    }
+}
